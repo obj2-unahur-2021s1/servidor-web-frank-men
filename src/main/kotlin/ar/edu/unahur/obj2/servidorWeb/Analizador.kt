e9b93880557f5b5bab0f008ea0ab07262b4c2657
FrancoRueta <askjfuhje@hotmail.com>
2021-06-22 16:27:04 -0300

    AGREGADO: -Segunda parte del analizador de ip's sospechosas. -Tests varios. CAMBIOS: -Ahora los analizadores tienen un mejor manejo de los modulos archivados. ERRORES: -PedidosDeLAIpAlModulo no anda, retorna 0.

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/Analizador.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/Analizador.kt
@@ -6,16 +6,28 @@ abstract class Analizador {
 
     fun agregarModulo(modulo: Modulo){ modulos.add(modulo) }
 
-    fun listaDeRespuestas(): List<Respuesta>{
+    fun listaDeRespuestasDel(modulo: Modulo): List<Respuesta> {
         val listaRespuestas = mutableListOf<Respuesta>()
-        for (mod in modulos){ listaRespuestas.addAll(mod.historialDeRespuestas) }
+        if (modulos.contains(modulo)) { listaRespuestas.addAll(modulo.historialDeRespuestas) }
         return listaRespuestas
     }
+    fun listaDeRespuestasTotal(): List<Respuesta>{
+        val respuestasTotales = mutableListOf<Respuesta>()
+        for(mod in modulos){respuestasTotales.addAll(listaDeRespuestasDel(mod))}
+        return respuestasTotales
+    }
 
-    fun listaDePedidos(): List<Pedido>{
+    fun listaDePedidosDel(modulo: Modulo): List<Pedido>{
         val listaPedidos = mutableListOf<Pedido>()
-        for (res in listaDeRespuestas()){ listaPedidos.add(res.pedido) }
+        if (modulos.contains(modulo)) {
+            for(resp in listaDeRespuestasDel(modulo)){ listaPedidos.add(resp.pedido) }
+        }
         return listaPedidos
+    }
+    fun listaDePedidosTotal(): List<Pedido>{
+        val pedidosTotales = mutableListOf<Pedido>()
+        for(mod in modulos){pedidosTotales.addAll(listaDePedidosDel(mod))}
+        return pedidosTotales
     }
 }
 
@@ -23,7 +35,7 @@ class AnalizadorDemora(val demoraMinima: Int): Analizador(){
     override val modulos = mutableSetOf<Modulo>()
 
     fun demoraDe(modulo: Modulo): Int {
-        val respuestasDelModulo = modulo.historialDeRespuestas
+        val respuestasDelModulo = listaDeRespuestasDel(modulo)
         return respuestasDelModulo.count { resp -> resp.tiempo > demoraMinima }
     }
 }
@@ -32,10 +44,30 @@ class AnalizadorIpSospechosa(val ipSospechosas: List<String>): Analizador(){
     override val modulos = mutableSetOf<Modulo>()
     val pedidos = mutableListOf<Pedido>()
 
-    fun pedidosDeLaIpSospechosa(ipSospechosa: String) = (listaDePedidos().count { p -> p.ip == ipSospechosa })
+    fun pedidosDeLaIpSospechosa(ipSospechosa: String) = (listaDePedidosTotal().count { p -> p.ip == ipSospechosa })
 
-    fun moduloMasConsultado(): Modulo{ //de TODAS las ip.
-        TODO("Todavia no")
+    fun pedidosDeLaIpAlModulo(modulo: Modulo,ipSospechosa: String): Int{
+        var pedidos = 0
+        if (modulos.contains(modulo)){
+            val pedidosDelModulo = listaDePedidosDel(modulo)
+            for (pedido in pedidosDelModulo){ if (pedido.ip == ipSospechosa){pedidos++} }
+        }
+        return pedidos }
+
+    fun moduloMasConsultadoPorIpSospechosas(): Modulo{
+        lateinit var moduloMasRequisitado: Modulo
+        var pedidoMaximo = 0
+        for(modulo in modulos){
+            var pedidosAlModulo = 0
+            //por cada ip: agregarle los pedidos de esa ip a ese modulo.
+            for(ip in ipSospechosas) {
+                pedidosAlModulo += pedidosDeLaIpAlModulo(modulo,ip)
+            }
+            //si la cantidad de pedidos al modulo es mayor a maxima actual, que el modulo mas
+            // requisitado sea la de este, y que el pedido maximo sea la maxima actual.
+            if(pedidosAlModulo > pedidoMaximo){moduloMasRequisitado = modulo;pedidoMaximo = pedidosAlModulo}
+        }
+        return moduloMasRequisitado
     }
 
     fun sospechosasEnRuta(ruta: String): List<String>{
